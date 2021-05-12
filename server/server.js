16f9ffee8506cb3f88a785431768793842f53334
Ojashri Basnyat <basnyat.ojashri@gmail.com>
2021-05-12 18:25:09 -0400

    config changes

--- server/server.js
+++ server/server.js
@@ -24,13 +24,21 @@ app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
+
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
+
+
 
 db.once('open', () => {
+  var PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
